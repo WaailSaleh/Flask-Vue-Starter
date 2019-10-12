8a815ddb3c3eb51040f023fc5dc1c89336385d37
abetlen <abetlen@gmail.com>
2019-10-12 13:15:26 -0400

    Removed example.

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,14 +4,9 @@ import json
 
 app = Flask(__name__)
 
-
 @app.route("/api/message")
 def get_message():
     return jsonify({
         "message": "Flask + VueJS"
     })
-auth= {"Authorization" :"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2l3wRoxfQ.IUJTu87WjXRqjCMAj0zhhYvX2sFGAiU1g3YIxc3tt2I"}
-@app.route("/api/p1")
-def get_p1():
-    response = requests.get("http://localhost:5000/DataSecure", headers=auth)
-    return json.dumps(response.json()), 200
+
