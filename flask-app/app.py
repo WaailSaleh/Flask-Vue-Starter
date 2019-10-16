b214bf71e62137fb1fb087f2909d6be695a6c6c1
Waail Saleh <waailsaleh@hotmail.com>
2019-10-16 10:29:40 -0400

    minor changes

--- flask-app/app.py
+++ flask-app/app.py
@@ -13,13 +13,14 @@ auth = {}
 token = None
 @app.route("/api/token")
 def get_token():
-    response = requests.post("http://localhost:6969/token", json={'user':'dev', 'pass':'secret'})
+    response = requests.post("http://localhost:8181/token", json={"user":"dev", "pass":"secret"})
     token = response.text
-    auth['Authorization'] = 'Bearer ' + token
+    auth['Authorization'] =  token
+    return auth['Authorization']
 
 @app.route("/api/p1")
 def get_p1():
     if token is None:
         get_token()
-    response = requests.get("http://localhost:6969/DataSecure", headers=auth)
+    response = requests.get("http://localhost:8181/DataSecure", headers=auth)
     return response.text, 200
